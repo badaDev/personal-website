0d6eb2cd89ad5db645719831e9350535842b5f35
nurudeenbada <nurudeen.bada@globalaccelerex.com>
2022-09-25 01:47:47 +0100

    update from angular 13 to 14

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations;
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations;
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidenavComponent } from './pages/shared/sidenav/sidenav.component';
 import { BodyComponent } from './pages/body/body.component';
+import { animate } from '@angular/animations';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { BodyComponent } from './pages/body/body.component';
   ],
   imports: [
     BrowserModule,
+    
     // BrowserAnimationsModule,
     
     AppRoutingModule
