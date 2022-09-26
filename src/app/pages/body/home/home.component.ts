3ba57031dc6b0aff9269f7ba9ac428396193ad23
nurudeenbada <nurudeen.bada@globalaccelerex.com>
2022-09-26 22:41:15 +0100

    home typewriter-effect

--- src/app/pages/body/home/home.component.ts
+++ src/app/pages/body/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import Typewriter from 't-writer.js';
+
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit(): void {
+    const target = document.querySelector('.tw');
+
+    const writer = new Typewriter(target, {
+      loop: true,
+      typeSpeed: 80,
+      deleteSpeed: 80,
+      typeColor: '#FF0063'
+    })
+    
+    writer
+      .type('Frontend Developer')
+      .rest(500)
+      .changeOps({ deleteSpeed: 80 })
+      .remove(18)
+      .type('Coding Enthusiast')
+      .rest(500)
+      .changeOps({ deleteSpeed: 20 })
+      .remove(17)
+      .type('Coach')
+      .rest(500)
+      .clear()
+      .start()
   }
 
 }
