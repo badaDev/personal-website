79a373c110f175c1b02716a88465b2baae3eb9e3
nurudeenbada <nurudeen.bada@globalaccelerex.com>
2022-10-15 16:16:01 +0100

    gallery page completed

--- src/app/pages/body/gallery/gallery.component.ts
+++ src/app/pages/body/gallery/gallery.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalleryComponent implements OnInit {
 
+  myImages = [
+    {
+      imgName: "../../../../assets/images/KAY_2057.jpg"
+    },
+    {
+      imgName: "../../../../assets/images/KAY_2059.jpg"
+    },
+    {
+      imgName: "../../../../assets/images/KAY_2080.jpg"
+    },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
