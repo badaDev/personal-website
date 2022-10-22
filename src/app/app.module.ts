42fd6a6ee3fb3bef615fa12233c6f42da62f4d44
nurudeenbada <nurudeen.bada@globalaccelerex.com>
2022-10-22 15:21:48 +0100

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidenavComponent } from './pages/shared/sidenav/sidenav.component';
+import { FooterComponent } from './pages/shared/footer/footer.component';
 import { BodyComponent } from './pages/body/body.component';
 import { animate } from '@angular/animations';
-import { FooterComponent } from './pages/shared/footer/footer.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     SidenavComponent,
     BodyComponent,
-    FooterComponent
+    // FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
